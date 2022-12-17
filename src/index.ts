42bdc6dd0c1c4427f07d346a8bcbc08ffc58bae0
elizavetaspivak <elizabethspivak99@gmail.com>
2022-12-17 19:44:20 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {videosRoute} from "./routes/videos-route";
 import bodyParser from "body-parser";
+import {deleteAllDataRoute} from "./routes/delete-all-data-route";
 
 const app = express()
 const port = 3000
@@ -11,6 +12,7 @@ const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
 app.use('/videos', videosRoute)
+app.use('/testing/all-data', deleteAllDataRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
