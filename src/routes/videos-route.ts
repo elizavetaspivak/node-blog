f65f36e2a88a907f30f7bec11557baed51289031
elizavetaspivak <elizabethspivak99@gmail.com>
2022-12-18 00:27:19 +0300

    init

--- src/routes/videos-route.ts
+++ src/routes/videos-route.ts
@@ -142,6 +142,10 @@ videosRoute.put('/:id', (req, res) => {
         })
     }
 
+    if (publicationDate && !/^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/.test(publicationDate)) {
+        errors.errorsMessages.push({message: 'Incorrect publicationDate', field: 'publicationDate'})
+    }
+
     if (errors.errorsMessages.length) {
         res.status(400).send(errors)
         return
