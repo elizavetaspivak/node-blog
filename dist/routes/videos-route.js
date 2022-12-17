f7d43394b5e7ea4a200472e530b3c3ba0a006a23
elizavetaspivak <elizabethspivak99@gmail.com>
2022-12-17 23:16:38 +0300

    init

--- dist/routes/videos-route.js
+++ dist/routes/videos-route.js
@@ -17,10 +17,42 @@ exports.videosRoute.get('/:id', (req, res) => {
     res.sendStatus(200).send(delete_all_data_route_1.videos);
 });
 exports.videosRoute.post('/', (req, res) => {
-    const video = delete_all_data_route_1.videos.find(v => v.id === +id);
-    if (!video) {
-        res.sendStatus(404);
+    let errors = {
+        errorsMessages: []
+    };
+    const title = req.body.title;
+    const author = req.body.author;
+    const availableResolutions = req.body.availableResolutions;
+    if (!title || title.length.trim() > 40) {
+        errors.errorsMessages.push({ message: 'Incorrect title', field: 'title' });
+    }
+    if (!author || author.length.trim() > 20) {
+        errors.errorsMessages.push({ message: 'Incorrect author', field: 'author' });
+    }
+    if (availableResolutions) {
+        availableResolutions.map((a) => {
+            !delete_all_data_route_1.AvailableResolutions[a] && errors.errorsMessages.push({
+                message: 'Incorrect availableResolutions',
+                field: 'availableResolutions'
+            });
+            return;
+        });
+    }
+    if (errors.errorsMessages.length) {
+        res.sendStatus(400).send(errors);
         return;
     }
-    res.sendStatus(200).send(delete_all_data_route_1.videos);
+    let createdAt = new Date();
+    const newVideo = {
+        id: delete_all_data_route_1.videos.length + 1,
+        canBeDownloaded: false,
+        minAgeRestriction: null,
+        createdAt: createdAt.toISOString(),
+        publicationDate: new Date(createdAt.getDate() + 1).toISOString(),
+        title,
+        author,
+        availableResolutions
+    };
+    delete_all_data_route_1.videos.push(newVideo);
+    res.sendStatus(201).send(newVideo);
 });
--- src/routes/videos-route.ts
+++ src/routes/videos-route.ts
@@ -1,6 +1,15 @@
 import {Router} from "express";
 import {AvailableResolutions, videos} from "./delete-all-data-route";
 
+type errorMessageType = {
+    message: string
+    field: string
+}
+
+type ErrorType = {
+    errorsMessages: errorMessageType[]
+}
+
 export const videosRoute = Router({})
 
 export type VideoType = {
@@ -31,13 +40,51 @@ videosRoute.get('/:id', (req, res) => {
     res.sendStatus(200).send(videos)
 })
 
-// videosRoute.post('/', (req, res) => {
-//     const video = videos.find(v => v.id === +id)
-//
-//     if (!video) {
-//         res.sendStatus(404)
-//         return
-//     }
-//
-//     res.sendStatus(200).send(videos)
-// })
+videosRoute.post('/', (req, res) => {
+    let errors: ErrorType = {
+        errorsMessages: []
+    }
+    const title = req.body.title
+    const author = req.body.author
+    const availableResolutions = req.body.availableResolutions
+
+    if (!title || title.length.trim() > 40) {
+        errors.errorsMessages.push({message: 'Incorrect title', field: 'title'})
+    }
+
+    if (!author || author.length.trim() > 20) {
+        errors.errorsMessages.push({message: 'Incorrect author', field: 'author'})
+    }
+
+    if (availableResolutions) {
+        availableResolutions.map((a: AvailableResolutions) => {
+            !AvailableResolutions[a] && errors.errorsMessages.push({
+                message: 'Incorrect availableResolutions',
+                field: 'availableResolutions'
+            })
+            return
+        })
+    }
+
+    if (errors.errorsMessages.length) {
+        res.sendStatus(400).send(errors)
+        return;
+    }
+
+    let createdAt = new Date()
+
+    const newVideo = {
+        id: videos.length + 1,
+        canBeDownloaded: false,
+        minAgeRestriction: null,
+        createdAt: createdAt.toISOString(),
+        publicationDate: new Date(createdAt.getDate() + 1).toISOString(),
+        title,
+        author,
+        availableResolutions
+    }
+
+    videos.push(newVideo)
+
+    res.sendStatus(201).send(newVideo)
+})
